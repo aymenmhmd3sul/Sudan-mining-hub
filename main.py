a231fe1e5d55ac48ad44aa8322ec3e8619085e55
AYMEN MHMD <aymenmhmd3sul@users.noreply.github.com>
2026-06-13 14:33:36 +0200

    isolate runtime - remove DB completely

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from fastapi import FastAPI
-import sqlite3
 
 app = FastAPI()
 
 @app.get("/api/v1/market/items")
 def get_items():
-    try:
-        conn = sqlite3.connect("local.db")
-        cursor = conn.cursor()
-
-        cursor.execute("SELECT * FROM market_items")
-        rows = cursor.fetchall()
-
-        return {
-            "count": len(rows),
-            "data": rows
-        }
-
-    except Exception as e:
-        return {"error": str(e)}
+    return {"status": "API IS RUNNING WITHOUT DB"}

4c55c49b471f2aaa530f9778126a644eac1ed487
AYMEN MHMD <aymenmhmd3sul@users.noreply.github.com>
2026-06-14 18:21:23 +0200

    test js

--- main.py
+++ main.py
@@ -5,75 +5,16 @@ app = FastAPI()
 
 @app.get("/dashboard", response_class=HTMLResponse)
 def dashboard():
-
     return """
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Test UI</title>
-
-<style>
-body {
-    font-family: Arial;
-    background:#0b1220;
-    color:white;
-    text-align:center;
-}
-
-.card {
-    margin:20px auto;
-    padding:20px;
-    width:200px;
-    background:#1f2937;
-    cursor:pointer;
-    border-radius:10px;
-}
-
-#modal {
-    display:none;
-    position:fixed;
-    top:0;left:0;
-    width:100%;
-    height:100%;
-    background:rgba(0,0,0,0.8);
-}
-
-.box {
-    background:#111827;
-    margin:20% auto;
-    padding:20px;
-    width:80%;
-    border-radius:10px;
-}
-</style>
-</head>
-
 <body>
 
-<h2>Dashboard Test</h2>
+<h1>JS TEST</h1>
 
-<div class="card" onclick="openModal()">
+<button onclick="alert('WORKING')">
 اضغط هنا
-</div>
-
-<div id="modal">
-    <div class="box">
-        <h3>Modal Works</h3>
-        <button onclick="closeModal()">Close</button>
-    </div>
-</div>
-
-<script>
-function openModal() {
-    document.getElementById("modal").style.display = "block";
-}
-
-function closeModal() {
-    document.getElementById("modal").style.display = "none";
-}
-</script>
+</button>
 
 </body>
 </html>

c61f4a8a1515e5f9fe4eb23932eb3a434d051807
AYMEN MHMD <aymenmhmd3sul@users.noreply.github.com>
2026-07-16 15:10:24 +0200

    Unify infrastructure database with local database

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+DATABASE_URL = "sqlite+aiosqlite:///./local.db"
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)

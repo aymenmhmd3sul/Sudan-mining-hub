77c307bad8737d7a00f5ba16d6e9ed6bb822c390
AYMEN MHMD <aymenmhmd3sul@users.noreply.github.com>
2026-07-27 11:43:37 +0200

    switch gateway-v2 to stable interface

--- app/routers/web.py
+++ app/routers/web.py
@@ -8,3 +8,13 @@ templates = Jinja2Templates(directory="app/templates")
 @router.get("/gateway", response_class=HTMLResponse)
 async def gateway(request: Request):
     return templates.TemplateResponse("gateway.html", {"request": request})
+
+
+@router.get("/gateway-v3", response_class=HTMLResponse)
+async def gateway_v3(request: Request):
+    return templates.TemplateResponse("gateway_v3.html", {"request": request})
+
+
+@router.get("/gateway-v2", response_class=HTMLResponse)
+async def gateway_v2(request: Request):
+    return templates.TemplateResponse("gateway_v2_stable.html", {"request": request})
